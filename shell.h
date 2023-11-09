3659afc40d72a5845e93198379dc673a1f219303
Raji Nurudeen <orizon.bot21@gmail.com>
2023-11-09 11:27:06 +0100

    Simple shell

---
+++ shell.h
@@ -0,0 +1,16 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdbool.h>
+#include <sys/wait.h>
+#include <stdbool.h>
+#include <sys/stat.h>
+
+void shell_interpreter(char *entry, size_t vol);
+void shell_display(void);
+
+#endif
