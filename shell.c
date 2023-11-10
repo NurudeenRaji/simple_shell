092eb1d0b16dd034df252126a973200d2dc85419
Raji Nurudeen <orizon.bot21@gmail.com>
2023-11-10 12:11:36 +0100

    Simple shell

--- shell.c
+++ shell.c
@@ -1,15 +1,18 @@
 #include "shell.h"
 
-int main(void)
+int main(int ac, char **av)
 {
 	char *input = NULL;
 	size_t size = 0;
+
+	(void)ac;
+	(void)av;
 
 	while (true)
 	{
 		shell_prompt();
 		shell_interpreter(input, size);
-		handle_path(input, size);
+		/*handle_path(input, size);*/
 	}
 
 	return (0);
