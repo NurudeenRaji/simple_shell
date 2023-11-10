ed9fb5f4d957c3a7c21be119162028860c163e10
Raji Nurudeen <orizon.bot21@gmail.com>
2023-11-10 13:21:36 +0100

    Simple shell

--- handle_path.c
+++ handle_path.c
@@ -1,51 +1,29 @@
 #include "shell.h"
 
-void handle_path(char *entry, size_t len);
+char *handle_path(char *entry);
 
-void handle_path(char *entry, size_t len)
+char *handle_path(char *entry)
 {
-	char *path, *dr, *entry[] = "";
-	size_t len = 0;
-	int status, flag = 1;
-	pid_t pid;
+	char *path, *dir, *new_path;
+	struct stat check;
 
-	while (flag == 1)
+
+	path = getenv("PATH");
+	dir = strtok(path, ":");
+		
+	while (dir != NULL)
 	{
-		shell_prompt();
-		getline(&entry, &len, stdin);
-		entry[strcspn(entry, '\n')] = '\0';
-		path = getenv("PATH");
-		dr =strtok(path, ":");
-		while (dr != NULL)
+		char *new_path = malloc(strlen(dir) + strlen(entry) + 2);
+		strcpy(new_path, dir);
+		strcat(new_path, "/");
+		strcat(new_path, entry);
+
+		if (stat(new_path, &check) == 0)
 		{
-			char *f_path= malloc(strlen(dr) + strlen(entry) + 2);
-			strcpy(f_path, dr);
-			strcat(f_path, "/");
-			strcat(f_path, entry);
-			if (access(f_path, X_OK) == 0)
-			{
-				pid = fork();
-				if (pid == -1)
-				{
-					perror("Error:");
-					return (1);
-				}
-				if (pid == 0)
-				{
-					if (execve(f_path, entry, NULL) == -1)
-					{
-						perror("Error:");
-					}
-				}
-				else
-				{
-					wait(&status);
-				}
-				free(f_path);
-				break;
-			}
 			free(f_path);
-			dr = strtok(NULL, ":");
+			break;
 		}
+		free(f_path);
+		dir = strtok(NULL, ":");
 	}
 }
