dd5590af4231a29126e59367a4f667d4c850e8f8
Raji Nurudeen <orizon.bot21@gmail.com>
2023-11-11 15:22:32 +0100

    Simple shell

--- shell_exit.c
+++ shell_exit.c
@@ -2,11 +2,18 @@
 
 void exit_shell(char **args)
 {
-	while (args != NULL && args[0] != NULL)
+	int status;
+
+	if (args != NULL && args[0] != NULL)
         {
 		if (strcmp(args[0], "exit") == 0)
 		{
-			exit(0);
+			status = 0;
+			if (args[1] != NULL)
+			{
+				status = atoi(args[1]);
+			}
+			exit(status);
 		}
 	}
 }
